619b0b192a0e66c16acf0e9b4430d56bd7a33746
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-12-18 16:25:14 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -71,7 +71,6 @@ const CaptureBirthdayIntentHandler = {
         const speakOutput = `Thanks, I'll remember that you were born ${month} ${day} ${year}.`;
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
 };
